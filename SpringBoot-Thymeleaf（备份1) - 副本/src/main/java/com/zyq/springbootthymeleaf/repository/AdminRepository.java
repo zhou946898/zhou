9fd4aac22e916f762d5697f9d55bb773d14b466c
zhou946898 <2229339794@qq.com>
2025-06-15 17:21:40 +0800

    Delete SpringBoot-Thymeleaf（备份1) - 副本 directory

--- SpringBoot-Thymeleaf（备份1) - 副本/src/main/java/com/zyq/springbootthymeleaf/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.zyq.springbootthymeleaf.repository;
-
-import com.zyq.springbootthymeleaf.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-    Admin findByUsername(String username);
-}
