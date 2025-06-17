3b478ec6034e95cb4400a360d782f5b8c6534802
zhou946898 <2229339794@qq.com>
2025-06-17 14:59:45 +0800

    Add files via upload

---
+++ SpringBoot-Thymeleaf（版本二） - 副本/src/main/java/com/zyq/springbootthymeleaf/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.zyq.springbootthymeleaf.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 配置图片资源映射
+        registry.addResourceHandler("/uploads/images/**")
+                .addResourceLocations("file:E:/桌面/javaEE/仿注册/备份/SpringBoot-Thymeleaf（版本二） - 副本/uploads/images/");
+
+        // 配置文件资源映射
+        registry.addResourceHandler("/uploads/files/**")
+                .addResourceLocations("file:E:/桌面/javaEE/仿注册/备份/SpringBoot-Thymeleaf（版本二） - 副本/uploads/files/");
+    }
+}
